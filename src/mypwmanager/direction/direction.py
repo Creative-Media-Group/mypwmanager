d0bbcfd99a2755be32bad54ffcb87d1cee7c24ad
tct123 <42028373+tct123@users.noreply.github.com>
2024-11-20 06:10:17 +0100

    Update direction.py

--- src/mypwmanager/direction/direction.py
+++ src/mypwmanager/direction/direction.py
@@ -2,7 +2,27 @@ import locale
 
 
 def text_direction(lang=locale.getlocale()[0].split("_")[0]):
-    print(lang)
+    rtl_list = [
+        "ar",
+        "fa",
+        "ur",
+        "ps",
+        "sd",
+        "ckb",
+        "prs",
+        "bal",
+        "ug",
+        "he",
+        "he",
+        "yi",
+        "lad",
+        "syc",
+        "dv",
+        "nqo",
+        "fuf",
+    ]
+    if lang in rtl_list:
+        return "rtl"
 
 
 if __name__ == "__main__":
