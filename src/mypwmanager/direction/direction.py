9a7f46eb7e02b40d294c8021125e33638e85c33c
tct123 <42028373+tct123@users.noreply.github.com>
2024-11-20 06:46:23 +0100

    Update direction.py

--- src/mypwmanager/direction/direction.py
+++ src/mypwmanager/direction/direction.py
@@ -1,7 +1,11 @@
 import locale
-from mylocale.TR import tr
+import csv
 
-def text_direction(lang=locale.getlocale()[0].split("_")[0]):
+
+def text_direction(
+    lang=locale.getlocale()[0].split("_")[0],
+    fp="src/mypwmanager/resources/localisation.csv",
+):
     rtl_list = [
         "ar",
         "fa",
@@ -21,7 +25,10 @@ def text_direction(lang=locale.getlocale()[0].split("_")[0]):
         "nqo",
         "fuf",
     ]
-    if lang in rtl_list:
+    with open(file=fp, mode="r", newline="", encoding="utf-8") as file:
+        locale_csv = csv.reader(file)
+        headings = next(locale_csv)
+    if lang in rtl_list and lang in headings:
         return "rtl"
     else:
         return "ltr"
